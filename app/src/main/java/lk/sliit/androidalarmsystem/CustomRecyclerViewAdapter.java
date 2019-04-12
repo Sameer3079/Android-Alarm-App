8e7751944b4b8b0f7e42cb57ba33334f12789a1d
Ssameer3079 <smrbasil4@gmail.com>
2019-04-12 17:58:24 +0530

    Changed Checkbox to Switch

--- app/src/main/java/lk/sliit/androidalarmsystem/CustomRecyclerViewAdapter.java
+++ app/src/main/java/lk/sliit/androidalarmsystem/CustomRecyclerViewAdapter.java
@@ -3,11 +3,13 @@ package lk.sliit.androidalarmsystem;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -18,19 +20,17 @@ import java.util.List;
 public class CustomRecyclerViewAdapter
         extends RecyclerView.Adapter<CustomRecyclerViewAdapter.ViewHolder> {
 
+    private final static String TAG = "APP - R_ViewAdapter";
+
     private List<Alarm> mData;
     private LayoutInflater mInflater;
     private ItemClickListener mClickListener;
-    private SharedPreferences sharedPreferences;
-    private SharedPreferences.Editor editor;
     private boolean isInitialized = false;
 
     // data is passed into the constructor
     CustomRecyclerViewAdapter(Context context, List<Alarm> data) {
         this.mInflater = LayoutInflater.from(context);
         this.mData = data;
-        sharedPreferences = context.getSharedPreferences(context.getString(R.string.shared_pref_name), Context.MODE_PRIVATE);
-        editor = sharedPreferences.edit();
     }
 
     // inflates the row layout from xml when needed
@@ -60,31 +60,32 @@ public class CustomRecyclerViewAdapter
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView nameTextView;
         TextView timeTextView;
-        CheckBox isEnabled;
+        Switch isEnabled;
 
         ViewHolder(View itemView) {
             super(itemView);
             nameTextView = itemView.findViewById(R.id.alarmName);
             timeTextView = itemView.findViewById(R.id.time);
-            isEnabled = itemView.findViewById(R.id.enabled);
+            isEnabled = itemView.findViewById(R.id.alarmEnableSwitch);
             isEnabled.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 @Override
                 public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                     if (isInitialized) {
-                        try {
-                            RecyclerView recyclerView = (RecyclerView) buttonView.getParent().getParent();
-                            int id = recyclerView.getChildAdapterPosition((View) buttonView.getParent());
-                            String rawData = sharedPreferences.getString("alarms", "[]");
-                            JSONArray alarmsArray = new JSONArray(rawData);
-                            Object tempObj = alarmsArray.get(id);
-                            JSONObject alarmObj = new JSONObject(tempObj.toString());
-                            alarmObj.put("isEnabled", !alarmObj.getBoolean("isEnabled"));
-                            alarmsArray.put(id, alarmObj.toString());
-                            editor.putString("alarms", alarmsArray.toString());
-                            editor.apply();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
+//                        try {
+//                            RecyclerView recyclerView = (RecyclerView) buttonView.getParent().getParent();
+//                            int id = recyclerView.getChildAdapterPosition((View) buttonView.getParent());
+//                            String rawData = sharedPreferences.getString("alarms", "[]");
+//                            JSONArray alarmsArray = new JSONArray(rawData);
+//                            Object tempObj = alarmsArray.get(id);
+//                            JSONObject alarmObj = new JSONObject(tempObj.toString());
+//                            alarmObj.put("isEnabled", !alarmObj.getBoolean("isEnabled"));
+//                            alarmsArray.put(id, alarmObj.toString());
+//                            editor.putString("alarms", alarmsArray.toString());
+//                            editor.apply();
+//                        } catch (Exception e) {
+//                            e.printStackTrace();
+//                        }
+                        Log.i(TAG, "Toggled Alarm");
                     } else {
                         isInitialized = true;
                     }
